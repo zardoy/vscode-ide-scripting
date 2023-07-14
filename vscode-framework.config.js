08d040a4671f4df2139d759fef904d8ad8c2d050
Vitaly Turovsky <vital2580@icloud.com>
2023-07-14 16:55:18 +0300

    ci: incorrectly patchec config

--- vscode-framework.config.js
+++ vscode-framework.config.js
@@ -7,19 +7,19 @@ patchPackageJson({})
 
 if (process.argv[2] !== 'build' && process.argv[2] !== 'start') {
     module.exports = {}
+} else {
+    module.exports = defineConfig({
+        esbuild: {
+            banner: {
+                js: `const __ESBUILD_BINARY_PATH = require('path').join(__dirname, process.platform === 'win32' ? 'esbuild.exe' : 'esbuild');__API_COMMANDS=${JSON.stringify(
+                    readFileSync('./out/api-commands.json', 'utf8'),
+                )}`,
+            },
+            // @ts-ignore
+            define: {
+                'process.env.ESBUILD_BINARY_PATH': '__ESBUILD_BINARY_PATH',
+                'process.env.ESBUILD_BUNDLED_VERSION': JSON.stringify(JSON.parse(readFileSync('./node_modules/esbuild/package.json', 'utf8')).version),
+            },
+        },
+    })
 }
-
-module.exports = defineConfig({
-    esbuild: {
-        banner: {
-            js: `const __ESBUILD_BINARY_PATH = require('path').join(__dirname, process.platform === 'win32' ? 'esbuild.exe' : 'esbuild');__API_COMMANDS=${JSON.stringify(
-                readFileSync('./out/api-commands.json', 'utf8'),
-            )}`,
-        },
-        // @ts-ignore
-        define: {
-            'process.env.ESBUILD_BINARY_PATH': '__ESBUILD_BINARY_PATH',
-            'process.env.ESBUILD_BUNDLED_VERSION': JSON.stringify(JSON.parse(readFileSync('./node_modules/esbuild/package.json', 'utf8')).version),
-        },
-    },
-})
